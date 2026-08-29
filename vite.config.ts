786a2f63a80c7085dda9ea153222ae99d6d1db8b
mitradeniz <mitradenizkaratas@gmail.com>
2026-08-29 15:42:44 +0300

    frontend update

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       input: {
-        home: resolve(import.meta.dirname, "index.html"),
+        index: resolve(import.meta.dirname, "index.html"),
         app: resolve(import.meta.dirname, "app/index.html"),
         blog: resolve(import.meta.dirname, "blog/index.html"),
         visualThinking: resolve(import.meta.dirname, "blog/visual-thinking.html"),
